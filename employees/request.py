705ddf1c8c7c589784412ef1fe9a31c20d5fccbb
nicoletatum <nicoletatum@outlook.com>
2021-04-12 09:51:00 -0500

    completed delete ch 5

--- employees/request.py
+++ employees/request.py
@@ -60,3 +60,18 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def delete_employee(id):
+    # Initial -1 value for employee index, in case one isn't found
+    employee_index = -1
+
+    # Iterate the EMPLOYEES list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            # Found the employee. Store the current index.
+            employee_index = index
+
+    # If the employee was found, use pop(int) to remove it from list
+    if employee_index >= 0:
+        EMPLOYEES.pop(employee_index)
